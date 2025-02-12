405b67d2188959ddbfa1b67353e7a7678257d43c
Rafael Silva <perigoso@riseup.net>
2025-02-12 11:24:00 +0000

    rvswd: implement rvswd dtm

---
+++ src/target/rvswd_dtm.c
@@ -0,0 +1,294 @@
+/*
+ * This file is part of the Black Magic Debug project.
+ *
+ * Copyright (C) 2025 1BitSquared <info@1bitsquared.com>
+ * Written by Rafael Silva <perigoso@riseup.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* This file implements RVSWD protocol support. TODO */
+
+#include "general.h"
+#include "platform.h"
+#include "rvswd.h"
+#include "riscv_debug.h"
+#include "maths_utils.h"
+#include "jep106.h"
+
+static void rvswd_wakeup_sequence(const bool stop_condition)
+{
+	/*
+	 * A wakeup sequence consist of 100 CLK cycles with DIO high followed by a STOP condition
+	 */
+	DEBUG_INFO("Performing RVSWD wakeup sequence %s stop condition\n", stop_condition ? "with" : "without");
+
+	platform_critical_enter();
+
+	/* 100 CLK cycles with DIO high */
+	rvswd_proc.seq_out(0xffffffffU, 32U); /* 32 */
+	rvswd_proc.seq_out(0xffffffffU, 32U); /* 64 */
+	rvswd_proc.seq_out(0xffffffffU, 32U); /* 96 */
+	rvswd_proc.seq_out(0xffffffffU, 4U);  /* 100 */
+
+	if (stop_condition)
+		rvswd_proc.stop(); /* STOP condition */
+
+	platform_critical_exit();
+}
+
+static bool rvswd_transfer_dmi_long(const uint8_t operation, const uint32_t address, const uint32_t value,
+	uint32_t *const result, uint8_t *const status)
+{
+	/*
+	 * RVSWD "Long" packet format:
+	 *
+	 * 
+	 */
+	// DEBUG_INFO("Performing RVSWD long DMI transfer: operation %d, address 0x%08" PRIx32 ", value 0x%08" PRIx32 "\n",
+	// 	operation, address, value);
+
+	/* RVSWD DTM Address space is limited to 7 bits */
+	if (address & ~0x7fU) {
+		DEBUG_ERROR("Address 0x%08" PRIx32 " is too large for RVSWD\n", address);
+		return false;
+	}
+
+	/* TODO: This is just experimental code, it should packed into 32-bit words for efficiency */
+
+	const uint8_t host_parity =
+		calculate_odd_parity(address & 0x7fU) ^ calculate_odd_parity(value) ^ calculate_odd_parity(operation & 0x3U);
+
+	/* Start condition */
+	rvswd_proc.start();
+
+	/* Host address */
+	rvswd_proc.seq_out(address & 0x7fU, 7U);
+
+	/* Host data */
+	rvswd_proc.seq_out(value, 32U);
+
+	/* Operation */
+	rvswd_proc.seq_out(operation & 0x3U, 2U);
+
+	/* Host parity */
+	rvswd_proc.seq_out(host_parity, 1U);
+
+	/* Target address */
+	const uint32_t target_address = rvswd_proc.seq_in(7U);
+
+	/* Target data */
+	const uint32_t target_data = rvswd_proc.seq_in(32U);
+
+	/* Status */
+	const uint32_t target_status = rvswd_proc.seq_in(2U);
+
+	/* Target parity */
+	const uint32_t target_parity = rvswd_proc.seq_in(1U);
+
+	/* Stop condition */
+	rvswd_proc.stop();
+
+	/* Check parity */
+	const uint8_t calculated_target_parity = calculate_odd_parity(target_address & 0x7fU) ^
+		calculate_odd_parity(target_data) ^ calculate_odd_parity(target_status & 0x3U);
+
+	if (target_parity != calculated_target_parity) {
+		DEBUG_ERROR("Parity error in RVSWD long transfer: expected %u, got %u\n", (uint8_t)target_parity,
+			(uint8_t)calculated_target_parity);
+		return false;
+	}
+
+	// DEBUG_INFO("RVSWD long DMI transfer result: status %u, address 0x%08" PRIx32 ", value 0x%08" PRIx32 "\n",
+	// 	(uint8_t)target_status, target_address, target_data);
+
+	if (result)
+		*result = target_data;
+	if (status)
+		*status = target_status;
+
+	return true;
+}
+
+static bool rvswd_riscv_dmi_read_long(riscv_dmi_s *const dmi, const uint32_t address, uint32_t *const value)
+{
+	uint8_t status = 0;
+	const bool result = rvswd_transfer_dmi_long(RV_DMI_OP_READ, address, 0, value, &status);
+
+	/* Translate error 1 into RV_DMI_FAILURE per the spec, also write RV_DMI_FAILURE if the transfer failed */
+	dmi->fault = !result || status == RV_DMI_RESERVED ? RV_DMI_FAILURE : status;
+	return dmi->fault == RV_DMI_SUCCESS;
+}
+
+static bool rvswd_riscv_dmi_write_long(riscv_dmi_s *const dmi, const uint32_t address, const uint32_t value)
+{
+	uint8_t status = 0;
+	const bool result = rvswd_transfer_dmi_long(RV_DMI_OP_WRITE, address, value, NULL, &status);
+
+	/* Translate error 1 into RV_DMI_FAILURE per the spec, also write RV_DMI_FAILURE if the transfer failed */
+	dmi->fault = !result || status == RV_DMI_RESERVED ? RV_DMI_FAILURE : status;
+	return dmi->fault == RV_DMI_SUCCESS;
+}
+
+static inline uint8_t rvswd_calculate_parity_short(const uint32_t value)
+{
+	/* Short packet parity is odd parity */
+	const uint8_t parity = calculate_odd_parity(value);
+	/* Short packet parity bit is duplicated for some strange reason */
+	return parity << 1U | parity;
+}
+
+static bool rvswd_transfer_dmi_short(
+	const bool write, const uint32_t address, const uint32_t value, uint32_t *const result, uint8_t *const status)
+{
+	/*
+	 * RVSWD "Short" packet format:
+	 *
+	 * 
+	 */
+	// DEBUG_INFO("Performing RVSWD short DMI transfer: %s, address 0x%08" PRIx32 ", value 0x%08" PRIx32 "\n",
+	// 	write ? "write" : "read", address, value);
+
+	/* RVSWD DTM Address space is limited to 7 bits */
+	if (address & ~0x7fU) {
+		DEBUG_ERROR("Address 0x%08" PRIx32 " is too large for RVSWD\n", address);
+		return false;
+	}
+
+	/* TODO: This is just experimental code, it should packed into 32-bit words for efficiency */
+
+	/* Host parity */
+	const uint8_t host_parity = rvswd_calculate_parity_short(((address & 0x7fU) << 1U) | (uint32_t)write);
+	uint32_t data_parity = write ? rvswd_calculate_parity_short(value) : 0U;
+
+	/* Start condition */
+	rvswd_proc.start();
+
+	/* Host address */
+	rvswd_proc.seq_out(address & 0x7fU, 7U);
+
+	/* Operation */
+	rvswd_proc.seq_out(write, 1U);
+
+	/* Host parity */
+	rvswd_proc.seq_out(host_parity, 2U);
+
+	/* 4 zero (padding?) bits */
+	rvswd_proc.seq_out(0U, 4U);
+
+	if (write) {
+		/* Host data */
+		rvswd_proc.seq_out(value, 32U);
+
+		/* Host data parity */
+		rvswd_proc.seq_out(data_parity, 2U);
+	} else {
+		/* Target data */
+		*result = rvswd_proc.seq_in(32U);
+
+		/* Target parity */
+		data_parity = rvswd_proc.seq_in(2U) & 0x3U;
+	}
+
+	/* Status (4 bits? also duplicated?) seems more like last 2 bits are padding 1 */
+	const uint32_t raw_status = rvswd_proc.seq_in(4U) & 0xfU;
+
+	/* Stop condition */
+	rvswd_proc.stop();
+
+	/* Check parity */
+	if (!write) {
+		const uint8_t calculated_data_parity = rvswd_calculate_parity_short(*result);
+		if (data_parity != calculated_data_parity) {
+			DEBUG_ERROR("Parity error in RVSWD short transfer: expected %u, got %u\n", (uint8_t)data_parity,
+				(uint8_t)calculated_data_parity);
+			return false;
+		}
+	}
+
+	/* discard the padding bits */
+	*status = raw_status >> 2U;
+
+	// DEBUG_INFO("RVSWD short DMI transfer result: status %u (raw %u), address 0x%08" PRIx32 ", value 0x%08" PRIx32 "\n",
+	// 	(uint8_t)*status, (uint8_t)raw_status, address, write ? value : *result);
+
+	return true;
+}
+
+static bool rvswd_riscv_dmi_read_short(riscv_dmi_s *const dmi, const uint32_t address, uint32_t *const value)
+{
+	uint8_t status = 0;
+	const bool result = rvswd_transfer_dmi_short(false, address, 0, value, &status);
+
+	/* Translate error 1 into RV_DMI_FAILURE per the spec, also write RV_DMI_FAILURE if the transfer failed */
+	dmi->fault = !result || status == RV_DMI_RESERVED ? RV_DMI_FAILURE : status;
+	return dmi->fault == RV_DMI_SUCCESS;
+}
+
+static bool rvswd_riscv_dmi_write_short(riscv_dmi_s *const dmi, const uint32_t address, const uint32_t value)
+{
+	uint8_t status = 0;
+	const bool result = rvswd_transfer_dmi_short(true, address, value, NULL, &status);
+
+	/* Translate error 1 into RV_DMI_FAILURE per the spec, also write RV_DMI_FAILURE if the transfer failed */
+	dmi->fault = !result || status == RV_DMI_RESERVED ? RV_DMI_FAILURE : status;
+	return dmi->fault == RV_DMI_SUCCESS;
+}
+
+static void rvswd_riscv_dtm_init(riscv_dmi_s *const dmi, const bool short_packets)
+{
+	/* WCH-Link doesn't have any mechanism to identify the DTM manufacturer, so we'll just assume it's WCH */
+	dmi->designer_code = JEP106_MANUFACTURER_WCH;
+	dmi->version = RISCV_DEBUG_NONSTANDARD; /* Not available, compatible with version 0.13 */
+
+	/* WCH-Link RVSWD has a fixed address width of 7 bits */
+	dmi->address_width = 7U;
+
+	if (short_packets) {
+		dmi->read = rvswd_riscv_dmi_read_short;
+		dmi->write = rvswd_riscv_dmi_write_short;
+	} else {
+		dmi->read = rvswd_riscv_dmi_read_long;
+		dmi->write = rvswd_riscv_dmi_write_long;
+	}
+
+	riscv_dmi_init(dmi);
+}
+
+static void riscv_rvswd_dtm_handler(const bool short_packets)
+{
+	riscv_dmi_s *const dmi = calloc(1, sizeof(*dmi));
+	if (!dmi) { /* calloc failed: heap exhaustion */
+		DEBUG_WARN("calloc: failed in %s\n", __func__);
+		return;
+	}
+
+	rvswd_riscv_dtm_init(dmi, short_packets);
+	/* If we failed to find any DMs or Harts, free the structure */
+	if (!dmi->ref_count)
+		free(dmi);
+}
